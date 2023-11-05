a6fd9db119290d33d4e02ed7d4d698ac7cc6cd78
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-11-05 11:37:53 -0500

    Add files via upload

--- UNGUARDED/Components/ProductCard.swift
+++ UNGUARDED/Components/ProductCard.swift
@@ -11,31 +11,46 @@ struct ProductCard: View{
     var product:Product
     
     var body: some View{
-        ZStack(alignment: .bottom){
-            Image(product.image)
-                .resizable()
-                .cornerRadius(0)
-                .frame(width: 250)
-                .scaledToFit()
-            
-            VStack(alignment:.leading){
-                Text(product.name)
-                    .bold()
+        ZStack(alignment:.topTrailing){
+            ZStack(alignment: .bottom){
+                Image(product.image)
+                    .resizable()
+                    .cornerRadius(0)
+                    .frame(width: 250)
+                    .scaledToFit()
                 
-                Text("\(product.price)$")
-                    .font(.caption)
+                VStack(alignment:.leading){
+                    Text(product.name)
+                        .bold()
+                    
+                    Text("\(product.price)$")
+                        .font(.caption)
+                }
+                .padding()
+                .frame(width:180,alignment: .leading)
+                .background(.ultraThinMaterial)
+                .cornerRadius(20)
+                
             }
-            .padding()
-            .frame(width:180,alignment: .leading)
-           
+            .frame(width: 180, height:250)
+            .shadow(radius:3)
+            
+            Button{
+                print("Added to cart!")
+            }label:{
+                Image(systemName:"plus")
+                    .padding(10)
+                    .foregroundColor(.white)
+                    .background(.black)
+                    .cornerRadius(50)
+                    .padding()
+            }
         }
-        .frame(width: 180, height:250)
-        .shadow(radius:3)
     }
-}
-
-struct ProductCard_Previews: PreviewProvider{
-    static var previews: some View{
-        ProductCard(product:productList[0])
+    
+    struct ProductCard_Previews: PreviewProvider{
+        static var previews: some View{
+            ProductCard(product:productList[0])
+        }
     }
 }
