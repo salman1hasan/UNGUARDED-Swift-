fd439036ecacd32bc052deb184409e9a6809e4fa
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-11-05 11:49:24 -0500

    Add files via upload

---
+++ UNGUARDED/Components/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  UNGUARDED
+//
+//  Created by Salman Hasan on 11/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+var columns=[GridItem(.adaptive(minimum:160),spacing: 20)]
+    var body: some View {
+        HStack{
+          ForEach(productList, id: \.id){ product in
+            ProductCard(product: product)
+          }
+        }
+        .padding()
+     }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
