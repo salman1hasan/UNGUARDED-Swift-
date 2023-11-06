a68e1f2d1c8028902f8adb14bddfc8ddbcd66087
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-11-06 10:01:54 -0500

    Add files via upload

--- UNGUARDED/Components/ProductCard.swift
+++ UNGUARDED/Components/ProductCard.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ProductCard: View{
+    @EnvironmentObject var cartManager: CartManager
     var product:Product
     
     var body: some View{
@@ -22,7 +23,7 @@ struct ProductCard: View{
                 VStack(alignment:.leading){
                     
                 }
-                .frame(width:180,alignment: .leading)
+                .frame(width:200,alignment: .leading)
                 .background(.ultraThinMaterial)
                 .cornerRadius(10)
                 
@@ -31,21 +32,21 @@ struct ProductCard: View{
             .shadow(radius:1)
             
             Button{
-                print("Added to cart!")
+            cartManager.addToCart(product: product)
             }label:{
                 Image(systemName:"plus")
                     .padding(10)
                     .foregroundColor(.white)
                     .background(.black)
-                    .cornerRadius(10)
-                    .padding()
+                
             }
         }
     }
+}
     
-    struct ProductCard_Previews: PreviewProvider{
+struct ProductCard_Previews: PreviewProvider{
         static var previews: some View{
-            ProductCard(product:productList[0])
-        }
+            ProductCard(product:productList[1])
+                .environmentObject(CartManager())
     }
 }
