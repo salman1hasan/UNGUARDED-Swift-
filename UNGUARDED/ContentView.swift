d871698936c302c8fcab682bd1abc9587314630d
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-11-06 07:59:26 -0500

    Add files via upload

--- UNGUARDED/ContentView.swift
+++ UNGUARDED/ContentView.swift
@@ -8,10 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+var columns=[GridItem(.adaptive(minimum:160),spacing: 20)]
+    
     var body: some View {
-            Text("Hello, world!")
-            .padding()
+        NavigationView{
+            HStack{
+                ForEach(productList, id: \.id){ product in
+                    ProductCard(product: product)
+                }
+            }
+            .navigationTitle(Text("UNGUARDED"))
         }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
