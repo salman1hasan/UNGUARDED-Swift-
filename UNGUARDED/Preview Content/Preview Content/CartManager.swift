fca7bbefca6285582c9fc3ba9013b697dbc992d9
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-11-06 12:28:12 -0500

    Add files via upload

--- UNGUARDED/Preview Content/Preview Content/CartManager.swift
+++ UNGUARDED/Preview Content/Preview Content/CartManager.swift
@@ -7,11 +7,17 @@
 
 import Foundation
 
-class CartManager: ObservableObject{
+class CartManager: ObservableObject {
     @Published private(set) var products: [Product] = []
     @Published private(set) var total: Int = 0
     
-    func addToCart(product:Product){
+    // Payment-related variables
+    let paymentHandler = PaymentHandler()
+    @Published var paymentSuccess = false
+    
+    
+    // Functions to add and remove from cart
+    func addToCart(product: Product) {
         products.append(product)
         total += product.price
     }
@@ -19,5 +25,14 @@ class CartManager: ObservableObject{
     func removeFromCart(product: Product) {
         products = products.filter { $0.id != product.id }
         total -= product.price
+    }
+    
+    // Call the startPayment function from the PaymentHandler. In the completion handler, set the paymentSuccess variable
+    func pay() {
+        paymentHandler.startPayment(products: products, total: total) { success in
+            self.paymentSuccess = success
+            self.products = []
+            self.total = 0
+        }
     }
 }
