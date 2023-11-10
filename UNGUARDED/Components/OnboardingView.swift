9aadb4be88ea6396fbfbc3d4f03e77b64c177773
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-11-10 14:51:19 -0500

    Add files via upload

--- UNGUARDED/Components/OnboardingView.swift
+++ UNGUARDED/Components/OnboardingView.swift
@@ -9,50 +9,50 @@ import SwiftUI
 
 struct OnboardingView: View {
     // MARK: - PROPERTY
-    
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
-    
     @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @State private var buttonOffset: CGFloat = 0
     @State private var isAnimating: Bool = false
     
-   
-    // MARK: - BODY
     
-    var body: some View {
-        ZStack {
-          Color("ColorBlack")
-             .ignoresSafeArea(.all, edges: .all)
-            
+   // MARK: - BODY
+    
+var body: some View {
+    ZStack {
+        Color("ColorBlack")
+            .ignoresSafeArea(.all, edges: .all)
+               
         VStack(spacing: 20) {
-                
             Spacer()
+            
+            NavigationView {
+                ZStack{
+                    Color("ColorBlack")
+                        .ignoresSafeArea(.all, edges: .all)
                 
-        ZStack {
-        Link(destination: URL(string:
-         "https://www.unguarded.com")!, label: {
-            Image("UNGUARDED")
-                .resizable()
-                .scaledToFit()
-                .opacity(isAnimating ? 1 : 0)
-                .animation(.easeOut(duration: 0.5), value: isAnimating)
-        })
+                
+                NavigationLink(destination:HomeView(), label:{
+                    Image("UNGUARDED")
+                        .resizable()
+                        .scaledToFit()
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 0.5), value: isAnimating)
+                })
+            }
     }
-            
-   Spacer()
-            
-   ZStack{
-    }
-          }
-       } //: HEADER
-        .onAppear(perform: {
-            isAnimating = true
-        })
-    }
+      ZStack{
+              }
+             }
+          } //: HEADER
+           .onAppear(perform: {
+               isAnimating = true
+           })
+       }
 }
         
 struct OnboardingView_Previews: PreviewProvider {
   static var previews: some View {
     OnboardingView()
+    
   }
 }
