93b693bf67e94c2874fb30aa24b74ad979bdb509
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-11-10 12:16:00 -0500

    Add files via upload

---
+++ UNGUARDED/Components/OnboardingView.swift
@@ -0,0 +1,58 @@
+//
+//  OnboardingView.swift
+//  UG
+//
+//  Created by Salman Hasan on 11/7/23.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    // MARK: - PROPERTY
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0
+    @State private var isAnimating: Bool = false
+    
+   
+    // MARK: - BODY
+    
+    var body: some View {
+        ZStack {
+          Color("ColorBlack")
+             .ignoresSafeArea(.all, edges: .all)
+            
+        VStack(spacing: 20) {
+                
+            Spacer()
+                
+        ZStack {
+        Link(destination: URL(string:
+         "https://www.unguarded.com")!, label: {
+            Image("UNGUARDED")
+                .resizable()
+                .scaledToFit()
+                .opacity(isAnimating ? 1 : 0)
+                .animation(.easeOut(duration: 0.5), value: isAnimating)
+        })
+    }
+            
+   Spacer()
+            
+   ZStack{
+    }
+          }
+       } //: HEADER
+        .onAppear(perform: {
+            isAnimating = true
+        })
+    }
+}
+        
+struct OnboardingView_Previews: PreviewProvider {
+  static var previews: some View {
+    OnboardingView()
+  }
+}
